ac164305c5a2fd9f3a4a0d6b3b09ccef77face14
MariaEduardaDeAzevedo <maria.silva@ccc.ufcg.edu.br>
2020-10-08 16:01:54 -0300

    Primeiro commit, adicionando projeto vindo de /Site

---
+++ src/enums/events.ts
@@ -0,0 +1,15 @@
+const events = [
+    {
+        title: "Café com Elas",
+        description: 'Edição do "Café com Elas" sobre trabalho no exterior e OpenSource na programação da Jornada Acadêmica Computação@UFCG.',
+        day: "08",
+        month: "10",
+        year: "2020",
+        time: "20h",
+        local: "Discord CCC",
+        pageURL: "",
+        eventURL: "",
+    },
+]
+
+export default events;
